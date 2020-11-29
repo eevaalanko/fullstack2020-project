1a1f6f3b7dc0bbc0b79667f5b8e48c1168b08edb
Eeva Alanko <eeva.alanko@siili.com>
2020-11-29 13:07:11 +0200

    configure dotenv MONGO_DB_URI

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
-const { AuthenticationError, UserInputError, ApolloServer, gql } = require('apollo-server')
-const { PubSub } = require('apollo-server')
-const pubsub = new PubSub()
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const {
+  AuthenticationError,
+  UserInputError,
+  ApolloServer,
+  gql,
+} = require("apollo-server");
+const { PubSub } = require("apollo-server");
+const pubsub = new PubSub();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = 'NEED_HERE_A_SECRET_KEY'
-const Challenge = require('./models/challenge')
-const User = require('./models/user')
-// TODO: change uri to env file....
-const MONGODB_URI =
-  'mongodb+srv://eeva:laikku111@cluster0.txhf8.mongodb.net/word-to-word?retryWrites=true&w=majority'
+const JWT_SECRET = "NEED_HERE_A_SECRET_KEY";
+const Challenge = require("./models/challenge");
+const User = require("./models/user");
 
-console.log('connecting to', MONGODB_URI)
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log("connecting to", MONGODB_URI);
 
 mongoose
   .connect(MONGODB_URI, {
@@ -21,11 +25,11 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+    console.log("error connection to MongoDB:", error.message);
+  });
 
 const typeDefs = gql`
   type Challenge {
@@ -54,93 +58,97 @@ const typeDefs = gql`
     createUser(username: String!, favoriteGenre: String): User
     login(username: String!, password: String!): Token
   }
-`
+`;
 
 const resolvers = {
   Query: {
     allChallenges: async () => {
-      return await Challenge.find({})
+      return await Challenge.find({});
     },
     findChallenge: (root, args) => Challenge.findOne({ name: args.name }),
     me: (root, args, context) => {
-      return context.currentUser
+      return context.currentUser;
     },
   },
   Mutation: {
-
     editChallenge: async (root, args) => {
       if (!args.name) {
-        throw new UserInputError('Missing name')
+        throw new UserInputError("Missing name");
       }
       if (args.name.length < 4) {
-        throw new UserInputError('Name input is too short')
+        throw new UserInputError("Name input is too short");
       }
-      const challenge = await Challenge.findOne({ name: args.name })
-      if ( !challenge) {
-        throw new UserInputError('Challenge not found')
+      const challenge = await Challenge.findOne({ name: args.name });
+      if (!challenge) {
+        throw new UserInputError("Challenge not found");
       }
-      challenge.description = args.description
+      challenge.description = args.description;
       try {
-        await challenge.save()
+        await challenge.save();
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        })
+        });
       }
-      return challenge
+      return challenge;
     },
     createChallenge: (root, args) => {
-      const challenge = new Challenge({ name: args.name, description: args.description || null  })
+      const challenge = new Challenge({
+        name: args.name,
+        description: args.description || null,
+      });
 
       return challenge.save().catch((error) => {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        })
-      })
+        });
+      });
     },
     createUser: (root, args) => {
-      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre || null  })
+      const user = new User({
+        username: args.username,
+        favoriteGenre: args.favoriteGenre || null,
+      });
 
       return user.save().catch((error) => {
         throw new UserInputError(error.message, {
           invalidArgs: args,
-        })
-      })
+        });
+      });
     },
     login: async (root, args) => {
-      const user = await User.findOne({ username: args.username })
+      const user = await User.findOne({ username: args.username });
 
-      if (!user || args.password !== 'secret') {
-        throw new UserInputError('wrong credentials')
+      if (!user || args.password !== "secret") {
+        throw new UserInputError("wrong credentials");
       }
 
       const userForToken = {
         username: user.username,
         id: user._id,
-      }
+      };
 
-      return { value: jwt.sign(userForToken, JWT_SECRET) }
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
     },
   },
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.toLowerCase().startsWith("bearer ")) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
       const currentUser = await User.findById(decodedToken.id).populate(
-        'friends'
-      )
-      return { currentUser }
+        "friends"
+      );
+      return { currentUser };
     }
   },
-})
-
+});
 
 server.listen().then(({ url, subscriptionsUrl }) => {
-  console.log(`Server ready at ${url}`)
-  console.log(`Subscriptions ready at ${subscriptionsUrl}`)
-})
+  console.log(`Server ready at ${url}`);
+  console.log(`Subscriptions ready at ${subscriptionsUrl}`);
+});
