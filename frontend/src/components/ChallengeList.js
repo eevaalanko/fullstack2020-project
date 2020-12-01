8998baf8b3cf118c653fa8950465868da2837f2c
Eeva Alanko <eeva.alanko@siili.com>
2020-12-01 14:20:20 +0200

    implement challenge list data table

--- frontend/src/components/ChallengeList.js
+++ frontend/src/components/ChallengeList.js
@@ -1,50 +1,87 @@
-import React from 'react'
-import { DataGrid } from '@material-ui/data-grid';
-import { useQuery } from '@apollo/client'
-import {Grid} from "@material-ui/core";
+import React from "react";
+import { useQuery } from "@apollo/client";
+import {
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+const createData = (id, name, description, duration) => {
+  return { id, name, description, duration };
+};
+
+// TODO: Get data from database
+const rows = [
+  createData(1, "The Plank Challenge", "...link to challenge", 30),
+  createData(2, "The Yoga Challenge", "...link to challenge", 30),
+  createData(3, "The Push up Challenge", "...link to challenge", 30),
+  createData(4, "The MindFullness Challenge", "...link to challenge", 30),
+  createData(5, "Your Custom Challenge", "...link to challenge", 30),
+];
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "description", headerName: "Description", width: 300 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: "duration",
+    headerName: "Duration",
+    type: "number",
     width: 90,
   },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-        `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-];
-
-const ChallengeList = ({username}) => {
-
-
+const ChallengeList = ({ username }) => {
+  const classes = useStyles();
   return (
     <div>
-      <p>Logged in as <b>{username}</b></p>
+      <p>
+        Logged in as <b>{username}</b>
+      </p>
       <h2>Challenges</h2>
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+      <div style={{ height: 400, width: "100%" }}>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <b>Name</b>
+                </TableCell>
+                <TableCell align="right">
+                  <b>Description</b>
+                </TableCell>
+                <TableCell align="right">
+                  <b>Duration</b>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    <Link to="/">{row.name}</Link>
+                  </TableCell>
+                  <TableCell align="right">{row.description}</TableCell>
+                  <TableCell align="right">{row.duration}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default ChallengeList
+export default ChallengeList;
