0522edb6db531c9cb2f1f2534b159739a08e8470
Eeva Alanko <eeva.alanko@siili.com>
2021-03-05 18:53:26 +0200

    uibuild

---
+++ backend/build/precache-manifest.c24c7df173dcee192dcacb83512ff325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6cb64407abdc768ccf53f0b554bed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d1d625a2b9a236fe692",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "9d1d625a2b9a236fe692",
+    "url": "/static/js/2.1494ec73.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/static/js/2.1494ec73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea723e4521fe448e489d",
+    "url": "/static/js/main.7ea1fb88.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "e0e8a1adf345eb171bc6302fd8247197",
+    "url": "/static/media/challengeApp.e0e8a1ad.png"
+  }
+]);
